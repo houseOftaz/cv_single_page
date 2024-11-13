01b59346446842d51359eb4ecfd13a54ca4e0b64
houseOftaz <davoz.anastasia@live.com>
2024-11-13 19:55:31 +0100

    update apos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function CV() {
   return (
-    <div className={`min-h-screen bg-gray-100 py-10 px-4 ${inter.className}`}>
+    <div className="min-h-screen bg-gray-100 py-10 px-4 font-sans">
       <div className="container mx-auto bg-gray-100 rounded-2xl shadow-xl p-8 max-w-4xl">
         <header className="flex items-center mb-8">
           <div className="mr-6">
@@ -20,16 +17,18 @@ export default function CV() {
             </div>
           </div>
           <div>
-            <h1 className="text-4xl font-bold text-gray-800 mb-2">
+            <h1 className="text-4xl font-bold text-gray-800 mb-2 font-courier">
               DAVOZ Anastasia
             </h1>
-            <p className="text-2xl text-gray-600">Apprentie développeur</p>
+            <p className="text-2xl text-gray-600 font-courier">
+              Apprentie développeur
+            </p>
           </div>
         </header>
 
         <main className="space-y-8">
           <section className="bg-white rounded-2xl p-6 shadow-[inset_-12px_-12px_24px_#ffffff,inset_12px_12px_24px_#d1d1d1]">
-            <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+            <h2 className="text-2xl font-semibold mb-4 text-gray-800 font-courier">
               Coordonnées
             </h2>
             <div className="space-y-2 text-gray-600">
@@ -41,34 +40,37 @@ export default function CV() {
           </section>
 
           <section className="bg-white rounded-2xl p-6 shadow-[inset_-12px_-12px_24px_#ffffff,inset_12px_12px_24px_#d1d1d1]">
-            <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+            <h2 className="text-2xl font-semibold mb-4 text-gray-800 font-courier">
               À propos de moi
             </h2>
             <p className="text-gray-600 mb-4">
-              Fasciné par l'univers numérique et ses possibilités infinies, j'ai
-              décidé depuis 2021 de me lancer dans cette voie.
+              Fasciné par l&apos;univers numérique et ses possibilités infinies,
+              j&apos;ai décidé depuis 2021 de me lancer dans cette voie.
             </p>
             <p className="text-gray-600 mb-4">
-              De nature créative, mon expérience de styliste modéliste m'a
-              permis de développer capacité d'adaptation et une gestion des
+              De nature créative, mon expérience de styliste modéliste m&apos;a
+              permis de développer capacité d&apos;adaptation et une gestion des
               besoins clients, des atouts précieux dans de nombreux domaines. Je
-              suis à la recherche d'une première expérience en alternance ou
-              stage pour progresser dans ce secteur.
+              suis à la recherche d&apos;une première expérience en alternance
+              ou stage pour progresser dans ce secteur.
             </p>
             <p className="text-gray-600 mb-4">
               Je cherche encore ma spécialisation, mais je suis convaincu que ma
-              passion pour les technologies, quelles qu'elles soient, m'aidera à
-              atteindre mon objectif de devenir un expert du numérique.
+              passion pour les technologies, quelles qu&apos;elles soient,
+              m&apos;aidera à atteindre mon objectif de devenir un expert du
+              numérique.
             </p>
           </section>
 
           <section className="bg-white rounded-2xl p-6 shadow-[inset_-12px_-12px_24px_#ffffff,inset_12px_12px_24px_#d1d1d1]">
-            <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+            <h2 className="text-2xl font-semibold mb-4 text-gray-800 font-courier">
               Mes Projets
             </h2>
             <div className="space-y-4">
               <div>
-                <h3 className="text-xl font-medium text-gray-800">Dev-js</h3>
+                <h3 className="text-xl font-medium text-gray-800 font-courier">
+                  Dev-js
+                </h3>
                 <p className="text-gray-600 mb-2">
                   Technologies utilisées : React, Next.js, TypeScript, Prisma,
                   Zod, Server Actions, Next Auth, Tailwind CSS, Shadcn-ui,
@@ -87,15 +89,16 @@ export default function CV() {
                 </p>
                 <p className="text-gray-600">
                   Ce projet collaboratif, mené avec un partenaire autodidacte,
-                  consiste à développer une plateforme d’e-learning accessible à
-                  tous. L’objectif est de renforcer nos compétences techniques
-                  tout en proposant un support pour accompagner les autres dans
-                  leur parcours d’apprentissage. Nous préparons des vidéos tuto
-                  ainsi que des exercices ludiques.
+                  consiste à développer une plateforme d&apos;e-learning
+                  accessible à tous. L&apos;objectif est de renforcer nos
+                  compétences techniques tout en proposant un support pour
+                  accompagner les autres dans leur parcours
+                  d&apos;apprentissage. Nous préparons des vidéos tuto ainsi que
+                  des exercices ludiques.
                 </p>
               </div>
               <div>
-                <h3 className="text-xl font-medium text-gray-800">
+                <h3 className="text-xl font-medium text-gray-800 font-courier">
                   Memory Game
                 </h3>
                 <p className="text-gray-600 mb-2">
@@ -114,19 +117,19 @@ export default function CV() {
                 </p>
                 <p className="text-gray-600">
                   Ce jeu de mémoire, développé intégralement pour mon examen,
-                  invite les utilisateurs à associer des paires de cartes. J'ai
-                  conçu ce projet de A à Z, en gérant la logique de jeu, la
-                  conception front-end, et l'intégration avec une base de
-                  données. Cette expérience a renforcé mes compétences en
-                  développement d'applications interactives et m'a permis
-                  d'appliquer diverses technologies modernes.
+                  invite les utilisateurs à associer des paires de cartes.
+                  J&apos;ai conçu ce projet de A à Z, en gérant la logique de
+                  jeu, la conception front-end, et l&apos;intégration avec une
+                  base de données. Cette expérience a renforcé mes compétences
+                  en développement d&apos;applications interactives et m&apos;a
+                  permis d&apos;appliquer diverses technologies modernes.
                 </p>
               </div>
             </div>
           </section>
 
           <section className="bg-white rounded-2xl p-6 shadow-[inset_-12px_-12px_24px_#ffffff,inset_12px_12px_24px_#d1d1d1]">
-            <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+            <h2 className="text-2xl font-semibold mb-4 text-gray-800 font-courier">
               Compétences
             </h2>
             <div className="flex flex-wrap gap-2">
@@ -151,20 +154,26 @@ export default function CV() {
           </section>
 
           <section className="bg-white rounded-2xl p-6 shadow-[inset_-12px_-12px_24px_#ffffff,inset_12px_12px_24px_#d1d1d1]">
-            <h2 className="text-2xl font-semibold mb-4 text-gray-800">
+            <h2 className="text-2xl font-semibold mb-4 text-gray-800 font-courier">
               Formation
             </h2>
             <div>
-              <h3 className="text-xl font-medium text-gray-800">
+              <h3 className="text-xl font-medium text-gray-800 font-courier">
                 Diplôme en Développement Web Full-Stack
               </h3>
               <p className="text-gray-600">3W Academy | 2024</p>
             </div>
             <div>
-              <h3 className="text-xl font-medium text-gray-800">
+              <h3 className="text-xl font-medium text-gray-800 font-courier">
                 Diplôme en Développement Web Mobile
               </h3>
               <p className="text-gray-600">Study | 2024</p>
+            </div>
+            <div>
+              <h3 className="text-xl font-medium text-gray-800 font-courier">
+                Diplôme en Stylisme Modélisme
+              </h3>
+              <p className="text-gray-600">MJM Graphic Design | 2016</p>
             </div>
           </section>
         </main>
